a1e2ead6bbc1c707ec02b6e26f134fb9c00a8581
Miguel Macias <miguelfernandomacias@gmail.com>
2020-12-22 17:40:51 -0400

    ADD:method api

--- app/controllers/apis_controller.rb
+++ app/controllers/apis_controller.rb
@@ -1,2 +1,22 @@
 class ApisController < ApplicationController
+	 include ActionController::HttpAuthentication::Basic::ControllerMethods
+    http_basic_authenticate_with name: "miguel", password: "12345", except: :index
+
+
+    def index
+        @apis= Api.all
+
+        render json: @apis
+    end
+
+    def show
+        @api = Api.find_by(date: params[:date])
+
+        if @price.nil?
+            render json: { message: 'No se encuentra el valor de la fecha ingresada' }
+        else 
+            render json: @api
+        end
+
+    end
 end
